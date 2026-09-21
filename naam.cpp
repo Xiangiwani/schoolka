25c09ee865b4a80fc448fccfa6eeedee138f7990
xiang <xiangvandemaaten@outlook.com>
2026-09-21 14:55:12 +0200

    Add basic C++ program to greet user by name

---
+++ naam.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+    string naam;
+
+    cout << "Wat is je naampje?"; 
+    cin >> naam;
+
+    cout << "Hey " << naam << " leuk je gekent te hebben";
+
+    return 0;
+
+}
